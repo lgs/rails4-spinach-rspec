77ba097a456dce91dbc9d648a5c5edf95d100535
Luca G. Soave <luca.soave@gmail.com>
2013-05-11 13:32:49 +0200

    first commit, adding spinach-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prova::Application.config.secret_key_base = 'd4ac680e28676a957928b6bdaca290879885d3016055f1c60127eb451adf910503a1fded751ff23aff68d1b4799c34e23c4541243136608761eca9016afc8db6'
